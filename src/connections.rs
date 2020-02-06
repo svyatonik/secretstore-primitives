4766a146ca9afc378c5e0ea28b427b9ba7f789c4
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-02-06 15:47:46 +0300

    message: Vec<u8> -> Header+Payload

--- src/connections.rs
+++ src/connections.rs
@@ -18,11 +18,12 @@ use std::{
 	collections::BTreeSet,
 	sync::Arc,
 };
+use serde::{Serialize, de::DeserializeOwned};
 use crate::{error::Error, KeyServerId};
 
 /// Connection to the single node. Provides basic information about connected node and
 /// allows sending messages to this node.
-pub trait Connection: Send + Sync {
+pub trait Connection<Message: Serialize + DeserializeOwned>: Send + Sync {
 	/// Is this inbound connection? This only matters when both nodes are simultaneously establishing
 	/// two connections to each other. The agreement is that the inbound connection from the node with
 	/// lower KeyServerId is used and the other connection is closed.
@@ -32,13 +33,13 @@ pub trait Connection: Send + Sync {
 	/// Returns 'address' of the node to use in traces.
 	fn node_address(&self) -> String;
 	/// Send message to the connected node.
-	fn send_message(&self, message: Vec<u8>);
+	fn send_message(&self, message: Message);
 }
 
 /// Connections manager. Responsible for keeping us connected to all required nodes.
-pub trait ConnectionManager: 'static + Send + Sync {
+pub trait ConnectionManager<Message: Serialize + DeserializeOwned>: 'static + Send + Sync {
 	/// Returns shared reference to connections provider.
-	fn provider(&self) -> Arc<dyn ConnectionProvider>;
+	fn provider(&self) -> Arc<dyn ConnectionProvider<Message>>;
 	/// Try to reach all disconnected nodes immediately. This method is exposed mostly for
 	/// tests, where all 'nodes' are starting listening for incoming connections first and
 	/// only after this, they're actually start connecting to each other.
@@ -49,12 +50,12 @@ pub trait ConnectionManager: 'static + Send + Sync {
 /// connect to. At any moment connection could be lost and the set of connected/disconnected
 /// nodes could change (at behalf of the connection manager).
 /// Clone operation should be cheap (Arc).
-pub trait ConnectionProvider: Send + Sync {
+pub trait ConnectionProvider<Message: Serialize + DeserializeOwned>: Send + Sync {
 	/// Returns the set of currently connected nodes. Error is returned when our node is
 	/// not a part of the cluster ('isolated' node).
 	fn connected_nodes(&self) -> Result<BTreeSet<KeyServerId>, Error>;
 	/// Returns the set of currently disconnected nodes.
 	fn disconnected_nodes(&self) -> BTreeSet<KeyServerId>;
 	/// Returns the reference to the active node connection or None if the node is not connected.
-	fn connection(&self, node: &KeyServerId) -> Option<Arc<dyn Connection>>;
+	fn connection(&self, node: &KeyServerId) -> Option<Arc<dyn Connection<Message>>>;
 }
